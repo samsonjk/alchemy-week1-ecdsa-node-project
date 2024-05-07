70e49896639ad86b381af4b7727b90a96e9e3092
Samson Jeeva <102836414+samsonjk@users.noreply.github.com>
2024-05-07 12:28:25 -0400

    Updates base url which is deployed to vercel app

--- client/src/server.js
+++ client/src/server.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const server = axios.create({
-  baseURL: "http://localhost:3042",
+  baseURL: "https://alchemy-week1-ecdsa-node-project-server.vercel.app",
 });
 
 export default server;
